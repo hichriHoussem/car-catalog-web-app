1d4fb9d3b0ce41b7097a4e367d67331f5673c280
HOUSSEM <houssem@apprentus.com>
2019-09-16 19:54:28 +0100

    fix cypress test/ first stable deploy

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,8 +1,8 @@
 describe('CRA', () => {
-  it('shows learn link', function() {
+  it('Should redirect to /new directory', function() {
     cy.visit('http://localhost:3000');
-    cy.get('.App-link')
-      .should('be.visible')
-      .and('have.text', 'Learn React');
+    cy.contains('Add a car').click();
+    cy.url() // 8.
+      .should('include', '/new');
   });
 });
