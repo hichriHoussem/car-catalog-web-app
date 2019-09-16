5ea4bd55b7641b818469188ff764eedc243248d5
HOUSSEM <houssem@apprentus.com>
2019-09-16 20:50:55 +0100

    new car test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -5,10 +5,47 @@ describe('New Car Form Test', () => {
     cy.url() // 8.
       .should('include', '/new');
   });
-  it('Should redirect to /new directory', function() {
+  it('Should display new cars form', function() {
     cy.get('.car-field')
       .first()
       .find('label')
       .should('contain', 'model');
+
+    cy.get('.car-field').should('have.length', 5);
+  });
+
+  it('Should be able to add a new car', function() {
+    cy.get('.car-field')
+      .first()
+      .find('input')
+      .type('new car');
+    cy.get('.car-field')
+      .eq(1)
+      .first()
+      .find('input')
+      .type('new car');
+    cy.get('.car-field')
+      .eq(3)
+      .first()
+      .find('input')
+      .type('200');
+    cy.get('.car-field')
+      .eq(4)
+      .first()
+      .find('input')
+      .type('image link');
+
+    cy.get('.car-action')
+      .eq(1)
+      .find('button')
+      .first()
+      .click();
+
+    cy.get('.list-element').should('contain', 'new car');
+    cy.url().should('include', '/car/1');
+
+    cy.visit('http://localhost:3000');
+    cy.get('.list-element').click();
+    cy.url().should('include', '/car/1');
   });
 });
