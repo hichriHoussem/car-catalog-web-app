630e8dc8bf486c901036da695f10e5c0646a8540
HOUSSEM <houssem@apprentus.com>
2019-09-16 20:07:28 +0100

    add cypress clear cache script

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,8 +1,14 @@
-describe('CRA', () => {
+describe('New Car Form Test', () => {
   it('Should redirect to /new directory', function() {
     cy.visit('http://localhost:3000');
     cy.contains('Add a car').click();
     cy.url() // 8.
       .should('include', '/new');
+  });
+  it('Should redirect to /new directory', function() {
+    cy.get('.car-field')
+      .first()
+      .find('label')
+      .should('contain', 'model');
   });
 });
